94ed524366419d2aa8609bf6b36a15861b716e2e
mikephu <mikephu03@gmail.com>
2023-10-20 02:04:50 -0700

    mongodb stuff

---
+++ db_connection_mongo.py
@@ -0,0 +1,114 @@
+#-------------------------------------------------------------------------
+# AUTHOR: Michael Phu
+# FILENAME: db_connection_mongo.py
+# SPECIFICATION: A program that communicates with a MongoDB database to create documents,
+#               delete documents, update documents, and return the inverted index of a collection.  
+# FOR: CS 4250 - Assignment #2
+# TIME SPENT: 1 Hour
+#-----------------------------------------------------------*/
+
+#importing some Python libraries
+from pymongo import MongoClient  # import mongo client to connect
+import string
+
+def connectDataBase():
+
+    # Creating an instance of mongoclient and informing the connection string
+    client = MongoClient(host=['localhost:27017'])
+    # Creating database
+    db = client.corpus
+
+    return db 
+
+def createDocument(db, docId, docText, docTitle, docDate, docCat):
+
+    # Get num_chars for doc text
+    num_chars = get_num_chars(docText)
+
+    # Put all terms in text into an array. " " is the delimiter. 
+    translator = str.maketrans('', '', string.punctuation)
+    term_list = [term.lower() for term in (str(docText).translate(translator)).split(' ')]
+
+    # Store term and occurrence count in a dictionary
+    dic1 = {}
+    for term in term_list: 
+        if term in dic1:
+            dic1[term] += 1
+        else:
+            dic1[term] = 1
+
+    # Add term dictionaries to the final "terms" field
+    terms = []
+    for term in dic1:
+        dic2 = {}
+        dic2["term"] = term
+        dic2["count"] = dic1[term]
+        dic2["num_chars"] = get_num_chars(term)
+        terms.append(dic2)
+        
+    # Parse date... 
+
+
+    # Create document object 
+    document = {
+        "doc_number": int(docId),
+        "text": docText, 
+        "title": docTitle,
+        "num_chars": int(num_chars),
+        "date": docDate,
+        "category": docCat,
+        "terms": terms 
+    }
+
+    # Insert document 
+    db.insert_one(document)
+
+def deleteDocument(db, docId):
+
+    # Delete the document from the database 
+    db.delete_one({"doc_number": int(docId)})
+
+def updateDocument(db, docId, docText, docTitle, docDate, docCat):
+
+    # Delete the document 
+    deleteDocument(db, docId)
+
+    # Create new document with updated information
+    createDocument(db, docId, docText, docTitle, docDate, docCat)
+
+def getIndex(db):
+
+    # Initialize index
+    index = {}
+
+    # Get all documents, exclude _id 
+    documents = list(db.find({},{"_id":0}))
+    
+    # Generate index 
+    for doc in documents:
+        
+        # Add term occurences to index 
+        for term_dic in doc["terms"]:
+            term = term_dic["term"]
+            count = term_dic["count"]
+            x = doc["title"] + ':' + str(count)
+            if term in index:
+                 index[term] += ',' + x
+            else:
+                 index[term] = x
+                 
+    return index
+   
+def get_num_chars(s): 
+
+    num_chars = 0 
+
+    for char in s:
+        if char.isalpha():
+            num_chars += 1
+    
+    return num_chars
+
+    
+
+
--- index_mongo.py
+++ index_mongo.py
@@ -1,27 +1,26 @@
 #-------------------------------------------------------------------------
 # AUTHOR: Michael Phu
-# FILENAME: index.py
-# SPECIFICATION: Driver program from db_connection.py.
+# FILENAME: index_mongo.py
+# SPECIFICATION: Driver program from db_connection_mongo.py.
 # FOR: CS 4250 - Assignment #2
-# TIME SPENT: 3 Hours
+# TIME SPENT: 1 Hour
 #-----------------------------------------------------------*/
 
-#importing some Python libraries
-from db_connection import *
+from pymongo import MongoClient  # import mongo client to connect
+from db_connection_mongo import *
 
 if __name__ == '__main__':
 
     # Connecting to the database
-    conn = connectDataBase()
+    db = connectDataBase()
 
-    # Getting a cursor
-    cur = conn.cursor()
-    cur.execute("SET search_path TO 'A2',public")
+    # Creating a collection
+    documents = db.documents
 
     #print a menu
     print("")
-    print("######### Menu ##############")
-    print("#a - Create a category.")
+    print("######### Menu #########")
+    #print("#a - Create a category.")
     print("#b - Create a document")
     print("#c - Update a document")
     print("#d - Delete a document.")
@@ -34,15 +33,7 @@ if __name__ == '__main__':
           print("")
           option = input("Enter a menu choice: ")
 
-          if (option == "a"):
-
-              catId = input("Enter the ID of the category: ")
-              catName = input("Enter the name of the category: ")
-
-              createCategory(cur, catId, catName)
-              conn.commit()
-
-          elif (option == "b"):
+          if (option == "b"):
 
               docId = input("Enter the ID of the document: ")
               docText = input("Enter the text of the document: ")
@@ -50,9 +41,9 @@ if __name__ == '__main__':
               docDate = input("Enter the date of the document: ")
               docCat = input("Enter the category of the document: ")
 
-              createDocument(cur, docId, docText, docTitle, docDate, docCat)
-              conn.commit()
-
+              createDocument(documents, docId, docText, docTitle, docDate, docCat)
+              print("Document created")
+          
           elif (option == "c"):
 
               docId = input("Enter the ID of the document: ")
@@ -61,26 +52,23 @@ if __name__ == '__main__':
               docDate = input("Enter the date of the document: ")
               docCat = input("Enter the category of the document: ")
 
-              updateDocument(cur, docId, docText, docTitle, docDate, docCat)
-
-              conn.commit()
+              updateDocument(documents, docId, docText, docTitle, docDate, docCat)
+              print("Document updated")
 
           elif (option == "d"):
 
               docId = input("Enter the document id to be deleted: ")
 
-              deleteDocument(cur, docId)
-
-              conn.commit()
+              deleteDocument(documents, docId)
+              print("Document deleted")
 
           elif (option == "e"):
 
-              index = getIndex(cur)
+              index = getIndex(documents)
               print(index)
 
           elif (option == "q"):
 
-               conn.close()
                print("Leaving the application ... ")
 
           else:
